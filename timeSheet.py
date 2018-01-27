cdf9b01f59f929729cf56474772b812850b9c5bb
Allen Joiner / Project Manager <ajoiner@PMWS140.CAMCOM>
2018-01-27 10:55:26 -0600

    git create branch DailySum

--- timeSheet.py
+++ timeSheet.py
@@ -1,5 +1,5 @@
 import sys
-from datetime import datetime
+from datetime import datetime, timedelta, date
 import requests
 
 def build_url(user, strt, finish): #build the request for the jira API
@@ -8,9 +8,17 @@ def build_url(user, strt, finish): #build the request for the jira API
     cstrt = strt.strftime('%Y%%2F%m%%2F%d')
     cfinish = finish.strftime('%Y%%2F%m%%2F%d')
     
-    qry = 'worklogAuthor%20%3D%20' + user + '%20AND%20worklogDate%20>%3D%20"' + cstrt + '"%20AND%20worklogDate%20<%3D%20"' + cfinish + '"&fields=worklog,labels'
+    qry = 'worklogAuthor%20%3D%20' + 'jhicks' + '%20AND%20worklogDate%20>%3D%20"' + cstrt + '"%20AND%20worklogDate%20<%3D%20"' + cfinish + '"&fields=worklog,labels'
     req = url + qry
     return req
+
+def build_date(pCode, begin, end):
+    """Builds dict with a list of dates and projects between begin and end"""
+    dates = {}
+    duration = int(str((end - begin).days))
+    for d in range(0, duration + 1):
+        dates[(begin + timedelta(days=d))] = pCode
+    return dates
 
 def main():
     """Main function"""
@@ -19,8 +27,9 @@ def main():
     passWrd = sys.argv[2]
     startDate = datetime.strptime(sys.argv[3], '%Y/%m/%d')
     finishDate = datetime.strptime(sys.argv[4], '%Y/%m/%d')
-    proj = ['ERDC2B', 'RIF', 'FinTech', 'ERDC', 'CBM', 'Point72', 'Fincantieri']
-    work = [0,0,0,0,0,0,0]
+    proj = [['ERDC2B', 0], ['RIF', 0], ['FinTech', 0], ['ERDC', 0], ['CBM', 0], ['Point72', 0], ['Fincantieri', 0]]
+    work = [0, 0, 0, 0, 0, 0, 0]
+    workLog = build_date(proj, startDate, finishDate)
 
     #request to get all issues with work logged between dates
     #query: worklogAuthor = jhicks AND worklogDate >= "2018/01/01" AND worklogDate <= "2018/01/15"
@@ -33,19 +42,20 @@ def main():
 
         #loop through the total number of work logs in the issue
         for y in range(0, numLogs):
-            cmtDate = datetime.strptime(resp['issues'][x]['fields']['worklog']['worklogs'][y]['created'][:10], '%Y-%m-%d')
+            cmtDate = datetime.strptime(resp['issues'][x]['fields']['worklog']['worklogs'][y]['started'][:10], '%Y-%m-%d')
 
             #check for only logs within the time window
             if cmtDate >= startDate and cmtDate <= finishDate:
                 lab = resp['issues'][x]['fields']['labels']
+                #Loop through the labels on the issue until we find one of the proj labels
                 for i in range(0, len(proj)):
                     for j in range(0, len(lab)):
-                        if lab[j] == proj[i]:
-                            #add time to correct project
-                            work[i] = work[i] + int(resp['issues'][x]['fields']['worklog']['worklogs'][y]['timeSpentSeconds'])
+                        if lab[j] == proj[i][0]:
+                            #add time to correct date and project in workLog
+                            workLog[cmtDate][i][1] += int(resp['issues'][x]['fields']['worklog']['worklogs'][y]['timeSpentSeconds'])
 
-    for num in range(0, len(proj)):
-        print('Project {0} has {1} seconds'.format(proj[num], work[num]))
+    for a in workLog:
+        print (a.strftime('%Y/%m/%d') + ': ' + str(workLog[a]))
 
 if __name__=='__main__':
     sys.exit(main())
